a787d6987c0ee250bae351d4ee1222537b3115a4
Eduardo F. P. Gomes <140530875+DuuArdo@users.noreply.github.com>
2023-08-16 07:26:41 -0300

    Add files via upload

---
+++ Primeiras atvds java/atividade7.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: "Dunha",
+    idade: 989,
+    profissao: "Caixista"
+};
+
+console.log("Nome: " + pessoa.nome);
+console.log("Idade: " + pessoa.idade);
+console.log("Profissão: " + pessoa.profissao);
